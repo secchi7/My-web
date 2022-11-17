5b88802dc1f4feee157798deac0ed04e743de4de
Andy <aasecchi@gmail.com>
2022-11-17 09:41:10 -0300

    Mejore el menu con js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
+
+document.querySelector(".nav-menu").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
